8170b19c35ca0b8dc9123ac19f668242c56299a0
Eduardo-SFeitosa <eduardo.sfeitosa03@gmail.com>
2026-05-31 15:25:20 -0300

    adicionada esferas de stack funcionais em ambiente 3d e seladas em uma caixa

--- src/cenas/orbe/esfera_stack.jsx
+++ src/cenas/orbe/esfera_stack.jsx
@@ -1,24 +1,40 @@
-import { useRef, useState } from "react"
+import { useState } from "react"
 import { Html } from "@react-three/drei"
+import { RigidBody } from "@react-three/rapier";
 
-export default function Esfera_stack({ nome , ...props}) {
+export default function Esfera_stack({ nome, cor , position = [0, 0, 0]}) {
 
-    const esfera_referencia = useRef()
-    const em_foco = useState(false)
-    const cor = props.cor
+    const [em_foco, set_em_foco] = useState(false)
 
     return (
         
-        <group>
+        <RigidBody
+        position={position}
+        colliders="ball"           // colisor esférico automático
+        restitution={0.7}         // quique
+        linearVelocity={[
+            (Math.random() - 0.5) * 2,  // velocidade aleatória inicial
+            (Math.random() - 0.5) * 2,
+            0,
+        ]}
+        >
 
-            <mesh ref={esfera_referencia}>
-                <sphereGeometry args={[1, 32, 32]} />
-                <meshStandardMaterial color={cor} />
-            </mesh>
+        <mesh
+            onPointerDown={() => set_em_foco(!em_foco)}
+        >
+            <sphereGeometry args={[2.5, 32, 32]} />
+            <meshStandardMaterial color={cor} />
+        </mesh>
 
-            { em_foco? <Html><h1>{nome}</h1></Html> : null}
+        {em_foco && (
+            <Html occlude>
+                <h1 style={{ color: "black", background: "white", padding: "2px 5px", width:"30px", height:"30px" }}>
+                    {nome}
+                </h1>
+            </Html>
+        )}
 
-        </group>
+        </RigidBody>
 
     )
 
--- src/cenas/orbe/interface_orbe.jsx
+++ src/cenas/orbe/interface_orbe.jsx
@@ -1,6 +1,8 @@
 import { Html } from "@react-three/drei";
 import { useState } from "react";
 import { Canvas } from "@react-three/fiber"
+import { Physics, RigidBody } from "@react-three/rapier";
+import { CameraControls } from "@react-three/drei";
 import "./interface_orbe.css"
 import Esfera_stack from "./esfera_stack";
 
@@ -32,60 +34,119 @@ export default function Interface_orbe({proximo_caminho, voltar_caminho , ...pro
 
         </div>
 
-        <Canvas className="canvas-orbe" camera={{ position: [0, 0, 5] }}>
+        <Canvas className="canvas-orbe" camera={{ position: [0, 0, -12] }}>
 
-            <ambientLight intensity={1} />
+            <ambientLight intensity={.1} />
 
-            {stack == "python" ?  
-            (<group className="conteudo-stack">
+            <pointLight 
+                position={[5, 5, -5]} 
+                intensity={Math.PI * 10} 
+                color="#ffffff" 
+                decay={2}
+            />
+
+
+            <Physics gravity={[0,0,0]}>
+
+                {/* paredes */}
+                <group>
                 
-                <Esfera_stack cor={"blue"} nome={"DJANGO e FASTAPI"}/>
-                <Esfera_stack cor={"blue"} nome={"PANDAS e NUMPY e MATPLOTLIB"}/>
-                <Esfera_stack cor={"blue"} nome={"REQUESTS / BEAULTIFULSOUP / SELENIUM"}/>
+                    {/* teto */}
+                    <RigidBody type="fixed" position={[0, 7, 0]}>
+                        <mesh color="red">
+                            <boxGeometry args={[38, .1, 5]} />
+                        </mesh>
+                    </RigidBody>
 
-            </group>)
-            : stack == "javascript" ? 
+                    {/* chao */}
+                    <RigidBody type="fixed" position={[0, -7, 0]}>
+                        <mesh color="red">
+                            <boxGeometry args={[38, .1, 5]} />
+                        </mesh>
+                    </RigidBody>
+
+                    {/* parede esquerda */}
+                    <RigidBody type="fixed" position={[19, 0, 0]}>
+                        <mesh color="red">
+                            <boxGeometry args={[.1, 15, 5]} />
+                        </mesh>
+                    </RigidBody>
+
+                    {/* parede direita */}
+                    <RigidBody type="fixed" position={[-19, 0, 0]}>
+                        <mesh color="red">
+                            <boxGeometry args={[.1, 15, 5]} />
+                        </mesh>
+                    </RigidBody>
+
+                    {/* parede frente */}
+                    <RigidBody type="fixed" position={[0, 0, 4]}>
+                        <mesh visible={false}>
+                            <boxGeometry args={[38, 15, .1]} />
+                        </mesh>
+                    </RigidBody>
+
+                    {/* parede tras */}
+                    <RigidBody type="fixed" position={[0, 0, -4]}>
+                        <mesh visible={false}>
+                            <boxGeometry args={[38, 15, .1]} />
+                        </mesh>
+                    </RigidBody>
+
+                </group>
+
+                {stack == "python" ?  
+                (<group className="conteudo-stack">
+                    
+                    <Esfera_stack cor={"blue"} nome={"DJANGO e FASTAPI"}/>
+                    <Esfera_stack cor={"blue"} nome={"PANDAS e NUMPY e MATPLOTLIB"}/>
+                    <Esfera_stack cor={"blue"} nome={"REQUESTS / BEAULTIFULSOUP / SELENIUM"}/>
+
+                </group>)
+                : stack == "javascript" ? 
                 (<group>
 
-                    <Esfera_stack cor={"blue"} nome={"THREE.JS"}/>
-                    <Esfera_stack cor={"blue"} nome={"REACT"}/>
-                    <Esfera_stack cor={"blue"} nome={"ROUTER"}/>
-                    <Esfera_stack cor={"blue"} nome={"ANGULAR"}/>
-                    <Esfera_stack cor={"blue"} nome={"NODE.JS"}/>
-                    <Esfera_stack cor={"blue"} nome={"TYPESCRIPT"}/>
-                    <Esfera_stack cor={"blue"} nome={"ANGULAR"}/>
-                    <Esfera_stack cor={"blue"} nome={"VITE"}/>
-                    
+                        <Esfera_stack cor={"blue"} nome={"THREE.JS"}/>
+                        <Esfera_stack cor={"blue"} nome={"REACT"}/>
+                        <Esfera_stack cor={"blue"} nome={"ROUTER"}/>
+                        <Esfera_stack cor={"blue"} nome={"ANGULAR"}/>
+                        <Esfera_stack cor={"blue"} nome={"NODE.JS"}/>
+                        <Esfera_stack cor={"blue"} nome={"TYPESCRIPT"}/>
+                        <Esfera_stack cor={"blue"} nome={"ANGULAR"}/>
+                        <Esfera_stack cor={"blue"} nome={"VITE"}/>
+                        
                 </group>)
 
-            : stack == "html/css" ? 
-            (<group className="conteudo-stack">
-                
-                <Esfera_stack cor={"blue"} nome={"FORMS"}/>
-                <Esfera_stack cor={"blue"} nome={"ANIMACOES"}/>
-                <Esfera_stack cor={"blue"} nome={"SASS"}/>
-                <Esfera_stack cor={"blue"} nome={"TAILWIND"}/>
+                : stack == "html/css" ? 
+                (<group className="conteudo-stack">
+                    
+                    <Esfera_stack cor={"blue"} nome={"FORMS"}/>
+                    <Esfera_stack cor={"blue"} nome={"ANIMACOES"}/>
+                    <Esfera_stack cor={"blue"} nome={"SASS"}/>
+                    <Esfera_stack cor={"blue"} nome={"TAILWIND"}/>
 
-            </group>)
+                </group>)
 
-            : stack == "sql" ? 
-            (<group className="conteudo-stack">
-                
-                <Esfera_stack cor={"blue"} nome={"SQLITE"}/>
-                <Esfera_stack cor={"blue"} nome={"MODELAGEM"}/>
-                <Esfera_stack cor={"blue"} nome={"JOINS"}/>
-                <Esfera_stack cor={"blue"} nome={"PROCEDURES"}/>
-                <Esfera_stack cor={"blue"} nome={"CONSULTAS OTIMIZADAS"}/>
+                : stack == "sql" ? 
+                (<group className="conteudo-stack">
+                    
+                    <Esfera_stack cor={"blue"} nome={"SQLITE"}/>
+                    <Esfera_stack cor={"blue"} nome={"MODELAGEM"}/>
+                    <Esfera_stack cor={"blue"} nome={"JOINS"}/>
+                    <Esfera_stack cor={"blue"} nome={"PROCEDURES"}/>
+                    <Esfera_stack cor={"blue"} nome={"CONSULTAS OTIMIZADAS"}/>
 
-            </group>)
+                </group>)
 
-            :(<group className="conteudo-stack">
-                
-                <Esfera_stack cor={"blue"} nome={"ASP.NET"}/>
-                <Esfera_stack cor={"blue"} nome={"WINDOWS FORMS"}/>
-                <Esfera_stack cor={"blue"} nome={"API REST"}/>
+                :(<group className="conteudo-stack">
+                    
+                    <Esfera_stack cor={"blue"} nome={"ASP.NET"}/>
+                    <Esfera_stack cor={"blue"} nome={"WINDOWS FORMS"}/>
+                    <Esfera_stack cor={"blue"} nome={"API REST"}/>
 
-            </group>)}
+                </group>)}
+
+            </Physics>
 
         </Canvas>
 
