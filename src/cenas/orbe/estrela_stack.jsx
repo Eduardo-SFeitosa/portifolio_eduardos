c0c3224a901e61352989a85bdd99a485fd3ddfc0
Eduardo-SFeitosa <eduardo.sfeitosa03@gmail.com>
2026-06-30 21:02:25 -0300

    estrelas se movimentam e adicionada conexao entre estrelas

--- src/cenas/orbe/estrela_stack.jsx
+++ src/cenas/orbe/estrela_stack.jsx
@@ -1,12 +1,36 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 import { Html } from "@react-three/drei"
 import * as THREE from "three"
+import { useFrame } from "@react-three/fiber"
 
 export default function Estrela_stack({ nome, cor, posicao }) {
 
     const [em_foco, set_foco] = useState(false)
 
+    const estrela = useRef(null)
+
+    const atraso_animacao = Math.random() * -.015
+
+    const movimento_lateral = Math.random() * .03
+
+    const fase = useRef(Math.random() * Math.PI * 2)
+
     const cores = ["red", "blue", "purple", "white", "green"]
+
+    useFrame(({clock}) => {
+
+        const delta = clock.getElapsedTime()
+
+        if (estrela.current && !em_foco) {
+
+        estrela.current.position.y = Math.sin(delta + fase.current) * atraso_animacao
+        
+        estrela.current.position.x = Math.sin(delta + fase.current) * movimento_lateral
+
+        }
+        
+    })
+
 
     return (
 
@@ -15,6 +39,7 @@ export default function Estrela_stack({ nome, cor, posicao }) {
             <mesh
                 onPointerOver={() => set_foco(true)}
                 onPointerOut={() => set_foco(false)}
+                ref={estrela}
             >
                 <sphereGeometry args={[0.015, 16, 16]} />
 
@@ -23,15 +48,20 @@ export default function Estrela_stack({ nome, cor, posicao }) {
                     emissive={cores[Math.floor(Math.random() * cores.length)]}
                     emissiveIntensity={10}
                 />
+
             </mesh>
       
             {em_foco && <Html
+                position={[0,.1,0]}
                 style={{ pointerEvents: "none" }}
                 scale={0.5}
+                style={{
+                        pointerEvents: "none"
+                }}
                 width={"fit-content"} height={"fit-content"}
                 style={{ position: "block" }}>
 
-                <h1 className="esfera-nome">
+                <h1 className="esfera-nome" style={{color:"purple"}}>
                     {nome}
                 </h1>
             </Html>}
--- src/cenas/orbe/interface_orbe.jsx
+++ src/cenas/orbe/interface_orbe.jsx
@@ -4,6 +4,7 @@ import { Canvas } from "@react-three/fiber"
 import { Physics, RigidBody } from "@react-three/rapier";
 import { Line } from "@react-three/drei"
 import { OrbitControls } from "@react-three/drei";
+import { useFrame } from '@react-three/fiber'
 
 import { Modelo_css } from "./modelos_orbe/Css.jsx";
 import { Modelo_html } from "./modelos_orbe/Html.jsx";
@@ -49,24 +50,32 @@ export default function Interface_orbe({proximo_caminho, voltar_caminho , ...pro
             cor: "#FFD54F",
 
             estrelas: [
-                { nome: "REACT", posicao: [0, 0.25, 0] },
-                { nome: "THREE.JS", posicao: [0.25, 0.10, 0] },
-                { nome: "VITE", posicao: [0.35, -0.15, 0] },
-                { nome: "NODE.JS", posicao: [0.05, -0.25, 0] },
-                { nome: "TYPESCRIPT", posicao: [-0.25, -0.15, 0] },
-                { nome: "ANGULAR", posicao: [-0.35, 0.05, 0] },
-                { nome: "ROUTER", posicao: [-0.15, 0.20, 0] },
+                { nome: "REACT", posicao: [-0.35, 0.35, 0] },
+                { nome: "ANGULAR", posicao: [0.35, 0.35, 0] },
+
+                { nome: "THREE.JS", posicao: [-0.15, 0.10, 0] },
+                { nome: "NODE.JS", posicao: [0, 0, 0] },
+                { nome: "TYPESCRIPT", posicao: [0.15, -0.10, 0] },
+
+                { nome: "ROUTER", posicao: [-0.30, -0.35, 0] },
+                { nome: "VITE", posicao: [0.30, -0.35, 0] },
             ],
 
             linhas: [
-                [0, 0.25, 0],
-                [0.25, 0.10, 0],
-                [0.35, -0.15, 0],
-                [0.05, -0.25, 0],
-                [-0.25, -0.15, 0],
-                [-0.35, 0.05, 0],
-                [-0.15, 0.20, 0],
-                [0, 0.25, 0],
+                [-0.35, 0.35, 0],
+                [-0.15, 0.10, 0],
+                [0, 0, 0],
+                [0.15, -0.10, 0],
+                [0.30, -0.35, 0],
+
+                [0.35, 0.35, 0],
+                [0.15, -0.10, 0],
+
+                [-0.15, 0.10, 0],
+                [0.15, -0.10, 0],
+
+                [-0.30, -0.35, 0],
+                [0, 0, 0]
             ]
         },
 
@@ -111,6 +120,8 @@ export default function Interface_orbe({proximo_caminho, voltar_caminho , ...pro
 
     }
 
+    const [linePoints, setLinePoints] = useState([])
+
     const atual = constelacoes[stack]
 
     return (
@@ -125,7 +136,7 @@ export default function Interface_orbe({proximo_caminho, voltar_caminho , ...pro
 
         <h1>STACKS</h1>
 
-        <Canvas className="canvas-orbe" camera={{ position: [0, 0, .5] }}>
+        <Canvas className="canvas-orbe" camera={{ position: [0, 0, .8] }}>
 
             <ambientLight intensity={1} />
 
@@ -136,43 +147,47 @@ export default function Interface_orbe({proximo_caminho, voltar_caminho , ...pro
             />
 
                 {/* SELECAO */}
-                <group>
+                <group position={[0,.2,0]}>
 
-                    <Modelo_css position={[ .2, .25 ,0]} onPointerDown={() => set_stack("html_css")}/>
-                    <Modelo_html position={[ .2, .15 ,0]} onPointerDown={() => set_stack("html_css")}/>
+                    <Modelo_css position={[ .25, .2 ,0]} onPointerDown={() => set_stack("html_css")}/>
+                    <Modelo_html position={[ .15, .2 ,0]} onPointerDown={() => set_stack("html_css")}/>
                     <Modelo_javascript position={[ -.2, .2 ,0]} onPointerDown={() => {set_stack("javascript")}}/>
                     <Modelo_python position={[ -.4, .2 ,0]} onPointerDown={() => {set_stack("python")}}/>
                     <Modelo_csharp position={[ .6, .2 ,0]} onPointerDown={() => {set_stack("csharp")}}/>
 
                 </group>
 
-                <group>
+                <group position={[0,-.1,0]}>
+                    
                     {/* CONSTELACOES */}
 
-                    {atual.estrelas.map((estrela) => (
+                    {atual.estrelas.map((estrela, i) => (
                         <Estrela_stack
                             key={estrela.nome}
                             nome={estrela.nome}
                             posicao={estrela.posicao}
                             cor={atual.cor}
+                            delay={i * 0.15}
                         />
                     ))}
+
+                    <Line
+                        points={atual.linhas}
+                        color={atual.cor}
+                        lineWidth={8}
+                        transparent
+                        opacity={0.15}
+                    />
+
+                    <Line
+                        points={atual.linhas}
+                        color={atual.cor}
+                        lineWidth={2}
+                    />
                     
                 </group>
 
-                <Line
-                    points={atual.linhas}
-                    color={atual.cor}
-                    lineWidth={8}
-                    transparent
-                    opacity={0.15}
-                />
 
-                <Line
-                    points={atual.linhas}
-                    color={atual.cor}
-                    lineWidth={2}
-                />
 
 
 
